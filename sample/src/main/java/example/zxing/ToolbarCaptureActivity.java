f9725e89629cd67d7f425b6f0df5089e803d4c59
mc0239 <martin.cebular@gmail.com>
2019-07-26 13:33:31 +0200

    Updated libraries to AndroidX

--- sample/src/main/java/example/zxing/ToolbarCaptureActivity.java
+++ sample/src/main/java/example/zxing/ToolbarCaptureActivity.java
@@ -1,9 +1,10 @@
 package example.zxing;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.journeyapps.barcodescanner.CaptureManager;
 import com.journeyapps.barcodescanner.DecoratedBarcodeView;
